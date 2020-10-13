b7638f14c0e71fa47305314c89cd94528f60ed4a
Jakob Waibel <jakob.waibel@web.de>
2020-10-14 00:44:46 +0200

    feat: implement querySessionData, queryCharactersData

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -7,7 +7,6 @@ import (
 	_ "github.com/lib/pq"
 )
 
-// database name can be adjusted as variable
 const (
 	host     = "localhost"
 	port     = 5432
@@ -16,13 +15,13 @@ const (
 	dbname   = "stripe-example"
 )
 
-// Session initializes session
+// Session initializes a session
 type Session struct {
 	sessionkey string
 	password   string
 }
 
-// Characters initializes character
+// Characters initializes a character
 type Characters struct {
 	name        string
 	player      string
@@ -39,10 +38,10 @@ type Characters struct {
 	edu         int
 	siz         int
 	inte        int
-	hitpoints   int
+	hitPoints   int
 	sanity      int
 	luck        int
-	magicpoints int
+	magicPoints int
 	diceroll    int
 }
 
@@ -63,86 +62,27 @@ func main() {
 
 	fmt.Println("Successfully connected!")
 
-	//	addSessionKey(db)
-	//	addCharacter(db)
-	updateDiceroll(db)
+	querySessionData(db)
+	queryCharactersData(db)
+
 }
 
 func addSessionKey(db *sql.DB) {
 	insertStatement := `
-	INSERT INTO session (
-		sessionkey, 
-		password
-		) 
-		VALUES (
-			'ASDE', 
-			'1234'
-			)`
+	INSERT INTO session (sessionkey, password) VALUES ('ASDE', '1234')`
 	_, err := db.Exec(insertStatement)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Added session")
 }
 
 func addCharacter(db *sql.DB) {
 	insertStatement := `
-	INSERT INTO characters (
-		name, 
-		player, 
-		occupation, 
-		age, 
-		sex, 
-		residence, 
-		birthplace, 
-		str, 
-		dex, 
-		pow, 
-		con, 
-		app, 
-		edu, 
-		siz, 
-		int, 
-		hitpoints, 
-		sanity, 
-		luck, 
-		magicpoints, 
-		diceroll
-		) 
-		VALUES (
-		'Oliver Smith',
-		'Jakob',
-		'Doctor',
-		36,
-		'Male',
-		'London',
-		'London',
-		50,
-		60,
-		40,
-		30,
-		50,
-		50,
-		7,
-		60,
-		3,
-		15,
-		13,
-		12,
-		100
-	)`
+	INSERT INTO characters (name, player, occupation, age, sex, residence, birthplace, str, dex, pow, con, app, edu, siz, int, hitpoints, sanity, luck, magicpoints, diceroll) VALUES ('Oliver Smith','Jakob','Doctor',36,'Male','London', 'London', 50, 60, 40, 30, 50, 50, 7, 60, 3, 15, 13, 12, 100)`
 	_, err := db.Exec(insertStatement)
 	if err != nil {
 		panic(err)
 	}
-}
-
-func getCharacter() {
-
-}
-
-func getSession() {
-
 }
 
 func updateDiceroll(db *sql.DB) {
@@ -153,10 +93,27 @@ func updateDiceroll(db *sql.DB) {
 	}
 }
 
-func removeSession(db *sql.DB) {
+func querySessionData(db *sql.DB) {
+	var mySession Session
+	userSQL := "SELECT sessionkey, password FROM session WHERE sessionkey = 'ASDE'"
 
+	err := db.QueryRow(userSQL).Scan(&mySession.sessionkey, &mySession.password)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s : %s\n", mySession.sessionkey, mySession.password)
 }
 
-func removeCharacter(db *sql.DB) {
+func queryCharactersData(db *sql.DB) {
+	var myCharacters Characters
+	// make WHERE condition to sessionkey with one to many relation
+	userSQL := "SELECT name, player, occupation, age, sex, residence, birthplace, str, dex, pow, con, app, edu, siz, int, hitpoints, sanity, luck, magicpoints, diceroll FROM characters WHERE name='Oliver Smith'"
 
+	err := db.QueryRow(userSQL).Scan(&myCharacters.name, &myCharacters.player, &myCharacters.occupation, &myCharacters.age, &myCharacters.sex, &myCharacters.residence, &myCharacters.birthplace, &myCharacters.str, &myCharacters.dex, &myCharacters.pow, &myCharacters.con, &myCharacters.app, &myCharacters.edu, &myCharacters.siz, &myCharacters.inte, &myCharacters.hitPoints, &myCharacters.sanity, &myCharacters.luck, &myCharacters.magicPoints, &myCharacters.diceroll)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%v : %v : %v : %v\n", myCharacters.name, myCharacters.player, myCharacters.occupation, myCharacters.age)
 }
