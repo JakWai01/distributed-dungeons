829d4ab1301aa5913b39b294feb2f2f9ba700851
Jakob Waibel <jakob.waibel@web.de>
2020-10-14 16:09:12 +0200

    feat: implement NewAPI

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -8,12 +8,29 @@ import (
 	"net/http"
 )
 
-// CALL QUERYDATA HERE AND MAKE IT TO API
 // Character defines article
 type Character struct {
-	Title   string `json:"Title"`
-	Desc    string `json:"desc"`
-	Content string `json:"content"`
+	name        string
+	player      string
+	occupation  string
+	age         int
+	sex         string
+	residence   string
+	birthplace  string
+	str         int
+	dex         int
+	pow         int
+	con         int
+	app         int
+	edu         int
+	siz         int
+	inte        int
+	hitpoints   int
+	sanity      int
+	luck        int
+	magicpoints int
+	diceroll    int
+	sessionkey  int
 }
 
 // Characters defines array of Article
@@ -21,8 +38,10 @@ type Characters []Character
 
 // Chars will add values to API
 func Chars(w http.ResponseWriter, r *http.Request) {
+
+	// For all characters with the same sessionkey, make a charakter in characters
 	articles := Characters{
-		Character{Title: "Test Title", Desc: "Test Desc", Content: "Test Content"},
+		Character{name: "Test Title", player: "Test Desc", occupation: "Test Content"},
 	}
 
 	fmt.Println("Endpoint Hit: All Articles Endpoint")
@@ -35,7 +54,17 @@ func handleRequests() {
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
-func main() {
+// call queryData to get characterinformatione
+// func main() {
+
+// 	handleRequests()
+// }
+
+// NewAPI initializes API values
+func NewAPI(name string, player string, occupation string, age int, sex string, residence string, birthplace string, str int, dex int, pow int, con int, app int, edu int, siz int, inte int, hitpoints int, sanity int, luck int, magicpoints int, diceroll int, sessionkey int) {
+	fmt.Println(Character{name, player, occupation, age, sex, residence, birthplace, str, dex, pow, con, app, edu, siz, inte, hitpoints, sanity, luck, magicpoints, diceroll, sessionkey})
 
 	handleRequests()
 }
+
+// Build function to set API data and call handleRequests. In Chars, then get all the char data from the matching session key and setup the api
