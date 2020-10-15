fd4d72fc398670e6b8e444d5a8ae1e9eae39f605
Jakob Waibel <jakob.waibel@web.de>
2020-10-15 18:52:05 +0200

    feat: added not null

--- pkg/sql/distributed_dungeons.sql
+++ pkg/sql/distributed_dungeons.sql
@@ -3,32 +3,32 @@ drop table if exists characters;
 drop table if exists sessions;
 -- Create new tables
 create table characters (
-    id serial,
-    name text,
-    player text,
-    occupation text,
-    age integer,
-    sex text,
-    residence text,
-    birthplace text,
-    strength integer,
-    dexterity integer,
-    power integer,
-    constitution integer,
-    appearance integer,
-    education integer,
-    size integer,
-    intelligence integer,
-    hitpoints integer,
-    sanity integer,
-    luck integer,
-    magic_points integer,
-    diceroll integer,
+    id serial not null,
+    name text not null,
+    player text not null,
+    occupation text not null,
+    age integer not null,
+    sex text not null,
+    residence text not null,
+    birthplace text not null,
+    strength integer not null,
+    dexterity integer not null,
+    power integer not null,
+    constitution integer not null,
+    appearance integer not null,
+    education integer not null,
+    size integer not null,
+    intelligence integer not null,
+    hitpoints integer not null,
+    sanity integer not null,
+    luck integer not null,
+    magic_points integer not null,
+    diceroll integer not null,
     session_id integer
 );
 create table sessions (
     id integer not null primary key,
-    password text
+    password text not null
 );
 -- Create test data
 insert into characters (
